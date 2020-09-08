94756e17e2f33760898fa46205e38947d30f611d
DanTheMan <danielturner184@gmail.com>
2020-09-08 11:49:44 -0400

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,19 @@
 # Create your CostumeStore class here
+# class CostumeStore < ActiveRecord::Base
+#     def change
+#         create_table :costume_store do |t|
+#             t.string :name
+#             t.string :location
+#             t.integer :number_of_costumes
+#             t.integer :number_of_employees
+#             t.string :whether_or_not_in_business
+#             t.integer :opening_time
+#             t.integer :closing_time
+
+#         end
+#     end
+
+# end
+class CostumeStore < ActiveRecord::Base
+    
+end 
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,21 @@
 # Create your HauntedHouse class here
+
+# class HauntedHouse < ActiveRecord::Base
+#     def change
+#         create_table :haunted_house do |t|
+#             t.string :name
+#             t.string :location
+#             t.string :theme
+#             t.integer :price
+#             t.string :whether_or_not_family_friendly
+#             t.integer :opening_time
+#             t.integer :closing_time
+#             t.string :description
+
+#         end
+#     end
+
+# end 
+class HauntedHouse < ActiveRecord::Base
+    
+end 
